5fc538d559136bfa1ab99192fb779d3899df82ab
Tim Joosten <Tjoosten3@gmail.com>
2016-09-13 08:36:32 +0200

    TicketsController -> added docblocks

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+/**
+ * Class TicketsController
+ * @package App\Http\Controllers
+ */
 class TicketsController extends Controller
 {
+    /**
+     * TicketsController constructor.
+     */
     public function __construct()
     {
-
+        $this->middleware('auth');
     }
+
 }
